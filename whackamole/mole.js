cc60ccfe526dc0e577f7c1334b9f2245b29fe009
Michael Granbäck <michael.granback@outlook.com>
2020-03-11 17:00:17 +0100

    Inget av värde

--- whackamole/mole.js
+++ whackamole/mole.js
@@ -1,1 +1,31 @@
 
+class Mole{
+
+constructor(x,y){
+  this.x = x;
+  this.y = y;
+  this.dir = -1;
+
+
+}
+
+logic(){
+
+  this.y+=this.dir;
+
+  if (this.y < 350 || this.y > 450) {this.dir*=-1;
+
+  }
+
+}
+show(){
+
+  textSize(33);
+  image(bild, this.x, this.y);
+  text('Want to play?',200,200);
+
+
+}
+
+
+}
--- whackamole/sketch.js
+++ whackamole/sketch.js
@@ -1,13 +1,20 @@
+var mulle;
+var bild;
+
 function preload(){
- 
+  bild = loadImage('https://image.shutterstock.com/image-vector/cartoon-funny-mole-260nw-456602506.jpg');
 }
+
+
 function setup() {
-  
-  
+  mulle = new Mole(200,400);
+
 }
 
 function draw() {
-  createCanvas(800,800);
-  background(0);
-  
+  createCanvas(600,600);
+  background(255);
+  mulle.logic();
+  mulle.show();
+
 }
