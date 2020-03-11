34c2b0cf27ff61d0aec0c55a1eb0b3a9400178cf
Michael Granbäck <michael.granback@outlook.com>
2020-03-11 17:25:47 +0100

    crilleshow

--- whackamole/sketch.js
+++ whackamole/sketch.js
@@ -1,8 +1,11 @@
 var mulle;
 var bild;
+var hammer;
+var rott = false;
 
 function preload(){
   bild = loadImage('https://image.shutterstock.com/image-vector/cartoon-funny-mole-260nw-456602506.jpg');
+  hammer = loadImage('https://thumbs.dreamstime.com/z/retro-cartoon-stone-hammer-texture-isolated-white-37598412.jpg');
 }
 
 
@@ -16,5 +19,28 @@ function draw() {
   background(255);
   mulle.logic();
   mulle.show();
+  switch (rott){
+    case false:
+    image(hammer,mouseX-50,mouseY-50, 100,100);
+      break;
+    case true:
+      push();
+      translate(mouseX,mouseY);
+      rotate(5);
+      image(hammer,-50,-50, 100,100);
+      pop();
+      break;
+    }
+
+
+
+}
+function mousePressed(){
+
+  rott = true;
+
+}
+function mouseReleased(){
+rott = false;
 
 }
