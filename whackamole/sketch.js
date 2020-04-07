0e6aa0ada486bde7f048068ad68be32e65b4bced
Michael Granbäck <michael.granback@outlook.com>
2020-04-07 16:15:17 +0200

    Deleting

--- whackamole/sketch.js
+++ whackamole/sketch.js
@@ -5,52 +5,43 @@ var mole_hands;
 var hammer;
 var rott = false;
 
-function preload(){
+function preload() {
   mole_head = loadImage("mole_head.png");
   mole_dirt = loadImage("mole_dirt.png");
   mole_hands = loadImage("mole_hands.png");
-  hammer = loadImage('hammer.png');
+  hammer = loadImage("hammer.png");
 }
 
-
 function setup() {
-  mulle = new Mole(50,100);
-
+  mulle = new Mole(50, 100);
 }
 
 function draw() {
-  createCanvas(600,600);
-  background(126,170,49);
+  createCanvas(900, 900);
+  background(126, 170, 49);
   mulle.logic();
   mulle.show();
-image(mole_dirt,30,100,150,100);
-  
-if(mulle.y < 100){
-    image(mole_hands,60,90,90,50);
+  image(mole_dirt, 30, 100, 150, 100);
 
+  if (mulle.y < 100) {
+    image(mole_hands, 60, 90, 90, 50);
   }
-  switch (rott){
+  switch (rott) {
     case false:
-    image(hammer,mouseX-50,mouseY-50, 100,100);
+      image(hammer, mouseX - 50, mouseY - 50, 100, 100);
       break;
     case true:
       push();
-      translate(mouseX,mouseY);
+      translate(mouseX, mouseY);
       rotate(5);
-      image(hammer,-50,-50, 100,100);
+      image(hammer, -50, -50, 100, 100);
       pop();
       break;
-    }
-
+  }
 }
-function mousePressed(){
-
+function mousePressed() {
   rott = true;
-  
-
 }
-function mouseReleased(){
-rott = false;
-
+function mouseReleased() {
+  rott = false;
 }
-
